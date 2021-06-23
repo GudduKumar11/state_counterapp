924477b47ba3ebf610c78b2325324d3a8bb91d9c
Guddu <60025420+GudduRangila@users.noreply.github.com>
2021-06-23 09:22:13 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+const App = () => {
+  const [count, setCount] = useState(0);
 
-function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Create Couter Appp with Hooks/state</h1>
+      <h2>The value of the count is {count}</h2>
+      <button onClick={() => (count >= 10 ? "" : setCount(count + 1))}>
+        Increamet
+      </button>
+      <button onClick={() => (count <= 0 ? "" : setCount(count - 1))}>
+        Decreament
+      </button>
+      <button onClick={() => setCount(0)}>Reset</button>
+      <button
+        onClick={() =>
+          count >= 100 ? "" : setCount(count + Math.floor(Math.random() * 10))
+        }
+      >
+        Random Number
+      </button>
     </div>
   );
-}
+};
 
 export default App;
